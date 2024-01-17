098e40859c17318873f5bda24cb15ef98ba64934
ingjeon <minjae77777@gmail.com>
2024-01-17 16:45:12 +0900

    [Fix] collection 수정
    url 수정

--- src/main/java/com/example/demo/collection/controller/CollectionController.java
+++ src/main/java/com/example/demo/collection/controller/CollectionController.java
@@ -20,13 +20,13 @@ import java.util.List;
 @RequestMapping("/collection")
 @CrossOrigin("*")
 @RequiredArgsConstructor
-@Api(value = "컬렉션 컨트롤러", tags = "컬렉션 API")
+@Api(value = "컬렉션 Controller", tags = "컬렉션 API")
 public class CollectionController {
     private final CollectionService collectionService;
 
     private CollectionRepository collectionRepository;
 
-    @ApiOperation(value = "컬렉션 생성")
+    @ApiOperation(value = "컬렉션 생성", notes = "사용자들이 각각의 컬렉션에 컨텐트를 추가한다.")
     @RequestMapping(method = RequestMethod.POST, value = "/create")
     public ResponseEntity create(@AuthenticationPrincipal Member member,
                                  @RequestBody CollectionCreateReq collectionCreateReq) {
@@ -34,20 +34,20 @@ public class CollectionController {
         return ResponseEntity.ok().body("in");
     }
 
-    @ApiOperation(value = "컬렉션 전체 조회")
+    @ApiOperation(value = "컬렉션 전체 조회", notes = "모든 컬렉션을 조회한다.")
     @RequestMapping(method = RequestMethod.GET, value = "/list")
     public ResponseEntity list() {
         return ResponseEntity.ok().body(collectionService.list());
     }
 
-    @ApiOperation(value = "컬렉션 상세 조회")
-    @GetMapping("/{title}")
+    @ApiOperation(value = "컬렉션 상세 조회", notes = "컬렌션 title로 컬렉션을 조회한다.")
+    @GetMapping("/detail/{title}")
     public ResponseEntity getCollection(@PathVariable String title) {
         return ResponseEntity.ok().body(collectionService.read(title));
-
     }
 
     //todo 검색
+//    @ApiOperation(value = "컬렉션 검색", notes = "컬렌션에서 작품, 유저등으로 컬렉션을 검색한다.")
     @GetMapping("/search")
     public ResponseEntity searchItem(@RequestParam("keyword") String keyword, Model model) {
         List<Collection> searchList = collectionService.search(keyword);
@@ -56,22 +56,21 @@ public class CollectionController {
     }
 
 
-
-    @ApiOperation(value = "컬렉션 수정")
+    @ApiOperation(value = "컬렉션 이름 수정", notes = "컬렌션의 title을 변경한다.")
     @RequestMapping(method = RequestMethod.PATCH, value = "/update")
     public ResponseEntity update(@RequestBody CollectionUpdateReq collectionUpdateReq) {
         collectionService.update(collectionUpdateReq);
         return ResponseEntity.ok().body("수정");
     }
 
-    @ApiOperation(value = "컬렉션 요소 중 컨덱트 하나 삭제")
+    @ApiOperation(value = "컬렉션 요소 중 컨텐트 하나 삭제", notes = "컬렌션의 컨텍트를 한개 삭제한다.")
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete")
     public ResponseEntity delete(Long idx) {
         collectionService.delete(idx);
         return ResponseEntity.ok().body("삭제");
     }
 
-    @ApiOperation(value = "컬렉션 요소 전체 삭제")
+    @ApiOperation(value = "컬렉션 요소 중 컨텐트 전체 삭제", notes = "컬렌션의 컨텍트를 전체 삭제한다.")
     @RequestMapping(method = RequestMethod.DELETE, value = "/deleteall")
     public ResponseEntity deleteAll(String title) {
         collectionService.deleteAll(title);
--- src/main/java/com/example/demo/collection/model/dto/CollectionUpdateReq.java
+++ src/main/java/com/example/demo/collection/model/dto/CollectionUpdateReq.java
@@ -1,6 +1,7 @@
 package com.example.demo.collection.model.dto;
 
 
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,6 +9,8 @@ import lombok.Data;
 @Builder
 @Data
 public class CollectionUpdateReq {
+    @ApiParam(value = "컨렉션의 id값 ", required = true, example = "")
     Long id;
+    @ApiParam(value = "컬렉션 이름", required = true, example = "나의 목록")
     String collectionTitle;
 }
