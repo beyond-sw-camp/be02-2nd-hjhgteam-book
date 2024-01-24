59538c31d153d7d3d3dc07b44f08668f29ac8afc
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-01-24 11:03:26 +0900

    [Feat] swagger 컨피그 수정
    swagger ui의 기본 오류 컨트롤러 제거

--- src/main/java/com/example/demo/common/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/common/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
@@ -25,7 +26,7 @@ public class SwaggerConfig {
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo("스프링 프로젝트", "v1.0"));
